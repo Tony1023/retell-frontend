6d301206a123135f0267d0f357586e330a88efb8
Tony Lu <tony1023lv@gmail.com>
2024-10-31 14:28:48 +0800

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import { RetellWebClient } from "retell-client-js-sdk";
+
+enum CallState {
+  notStarted = 1,
+  inProgress,
+  ended
+}
+
+interface Message {
+  role: string,
+  content: string,
+}
 
 function App() {
+
+  const [callState, setCallState] = useState<CallState>(CallState.notStarted);
+  const [prompt, setPrompt] = useState('');
+  const [transcript, setTranscript] = useState<Message[]>();
+  const retellClient = useRef(new RetellWebClient());
+
+  retellClient.current.on('update', (update) => {
+    setTranscript(update.transcript);
+  });
+
+  const startConversation = async () => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    const res = await fetch('http://localhost:8080/token', {
+      method: 'POST',
+      body: JSON.stringify({
+        prompt: prompt
+      }),
+      headers: headers,
+    });
+    const result = await res.json();
+    await retellClient.current.startCall({ accessToken: result.accessToken });
+    setCallState(CallState.inProgress);
+  }
+
+  const stopConversation = () => {
+    retellClient.current.stopCall();
+    setCallState(CallState.ended);
+  }
+
+  const renderButton = () => {
+    switch (callState) {
+      case CallState.notStarted:
+        return <button className='start-btn' onClick={startConversation}>Start Conversation</button>;
+      case CallState.inProgress:
+        return <button className='stop-btn' onClick={stopConversation}>Stop Conversation</button>
+      case CallState.ended:
+        return <></>
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Enter a symstem prompt for the AI</h1>
+      <textarea
+        content={prompt}
+        disabled={callState !== CallState.notStarted}
+        onChange={(e) => {
+          setPrompt(e.target.value);
+        }} />
+      <div>
+        {renderButton()}
+      </div>
+      {
+        callState === CallState.inProgress ? <h2>Conversation in progress...</h2> : <></>
+      }
+      {
+        callState === CallState.ended ? <h2>Conversation ended.</h2> : <></>
+      }
+      <div>
+        {
+          transcript?.map((message: Message) => {
+            return (
+              <div>
+                <b>{message.role === 'agent' ? 'Agent: ' : 'You: '}</b>{message.content}
+              </div>
+            );
+          })
+        }
+      </div>
+    </>
   );
 }
 
